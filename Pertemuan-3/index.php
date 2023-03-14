a00b39ef33cbd4c11bd0aef37d993da766420c9e
Riyan <riyanmaulana402@yahoo.co.id>
2023-03-14 23:42:17 +0700

    add pertemuan 3

--- Pertemuan-3/index.php
+++ Pertemuan-3/index.php
@@ -5,19 +5,50 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Belajar PHP</title>
 </head>
 
 <body>
     <?php
     $jenis_kelamin = 'Laki-laki';
-    $nama_lengkap = 'Riyanada';
+    $nama_lengkap = 'Riyan';
     $umur = 23;
     $berat = 56;
     $tinggi = 160;
     $menikah = false;
+    $nilai = 87;
+
+    if ($nilai >= 85 && $nilai <= 100) {
+        $huruf = "A";
+        $keterangan = "Istimewa";
+    } else if ($nilai >= 80 && $nilai <= 84) {
+        $huruf = "AB";
+        $keterangan = "Lebih dari Cukup";
+    } else if ($nilai >= 75 && $nilai <= 79) {
+        $huruf = "B";
+        $keterangan = "Baik";
+    } else if ($nilai >= 70 && $nilai <= 74) {
+        $huruf = "BC";
+        $keterangan = "Lebih dari Cukup";
+    } else if ($nilai >= 60 && $nilai <= 69) {
+        $huruf = "C";
+        $keterangan = "Cukup";
+    } else if ($nilai >= 50 && $nilai <= 59) {
+        $huruf = "CD";
+        $keterangan = "Kurang dari Cukup";
+    } else if ($nilai >= 40 && $nilai <= 49) {
+        $huruf = "D";
+        $keterangan = "Kurang";
+    } else if ($nilai <= 39) {
+        $huruf = "E";
+        $keterangan = "Gagal";
+    } else {
+        $huruf = "Nilai tidak terdefinisi. ";
+        $keterangan = '';
+    }
     ?>
-    <table border="1" style="border-collapse: collapse;">
+    <h1>Data Diri Mahasiswa</h1>
+    <table border="2" style="border-collapse: collapse; margin-left: 2%;">
         <tr>
             <td>Nama</td>
             <td>:</td>
@@ -49,35 +80,46 @@
         </tr>
         <tr>
             <td>
-                Berat
-            </td>
-            <td>
-                :
-            </td>
-            <td>
-                <?php echo $berat; ?>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                Tinggi
-            </td>
-            <td>
-                :
-            </td>
-            <td>
-                <?php echo $tinggi; ?>
-            </td>
-        </tr>
-        <tr>
-            <td>
                 Status Pernikahan
             </td>
             <td>
                 :
             </td>
             <td>
-                <?= ($menikah) ? "Kawin" : "Belum Kawin" ;?>
+                <?= ($menikah) ? "Kawin" : "Belum Kawin"; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Nilai
+            </td>
+            <td>
+                :
+            </td>
+            <td>
+                <?php echo $nilai; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Huruf
+            </td>
+            <td>
+                :
+            </td>
+            <td>
+                <?php echo $huruf; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Keterangan
+            </td>
+            <td>
+                :
+            </td>
+            <td>
+                <?= $keterangan;?>
             </td>
         </tr>
     </table>
